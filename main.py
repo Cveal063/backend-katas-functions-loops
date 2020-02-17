0470f9a1a69794bd6f504b76feb61280d4286435
christopher <Cveal063@icloud.com>
2020-02-17 16:16:18 -0500

    done

--- main.py
+++ main.py
@@ -6,34 +6,49 @@ __author__ = "???"
 
 def add(x, y):
     """Add two integers. Handles negative values."""
-    # your code here
-    return
+    return x + y
 
 
 def multiply(x, y):
     """Multiply x with y. Handles negative values of x or y."""
-    # your code here
-    return
+    result = 0 
+    for i in range(abs(y)):
+        result = add(result, x)
+    if y < 0 or x < 0:
+        result = -abs(result)
+    return result
 
 
 def power(x, n):
     """Raise x to power n, where n >= 0"""
-    # your code here
-    return
+    result = x 
+    if n >= 0:
+        arr = range(1, n)
+        for i in arr:
+            result = multiply(result, x)
+        return result
 
 
 def factorial(x):
     """Compute factorial of x, where x > 0"""
-    # your code here
-    return
-
+    result = 1
+    for i in range(1, x+1): 
+            result = multiply(result, i)
+    return result
 
 def fibonacci(n):
     """Compute the nth term of fibonacci sequence"""
-    # your code here
-    return
+    arr = [0,1,1]
+    for i in range(2,n): 
+        arr.append(arr[-1] + arr[-2])
+    return arr[n-1]
 
+def data():
+    print(add(2,4)) 
+    print(multiply(6,-8))
+    print(power(3,4))
+    print(factorial(4))
+    print(fibonacci(8))
 
 if __name__ == '__main__':
-    # your code to call functions above
-    pass
+    data()
